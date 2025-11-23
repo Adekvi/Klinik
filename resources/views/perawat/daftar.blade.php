1f64bdb89e446e66aa782cde025b4e4ff031ec77
doney05 <donysetiawan192@gmail.com>
2025-11-23 18:25:38 +0700

    Update Perawan

--- resources/views/perawat/daftar.blade.php
+++ resources/views/perawat/daftar.blade.php
@@ -1329,7 +1329,7 @@
                         errorMessages.push("- No. BPJS harus berisi 13 digit angka.");
                     }
 
-
+//perubahannya disini
                     if (errorMessages.length > 0) {
                         if (loadingSpinner) loadingSpinner.classList.add('d-none');
                         if (btnSimpan) btnSimpan.disabled = false;
